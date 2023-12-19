915be1ab1f2d0b6fff4c5a781722f37cb8c0e488
JustAnAverageGuy <68919330+JustAnAverageGuy@users.noreply.github.com>
2023-12-19 17:43:21 +0530

    Update python run command

--- lua/custom/plugins/compitest.lua
+++ lua/custom/plugins/compitest.lua
@@ -2,5 +2,11 @@ return {
   "xeluxee/competitest.nvim",
   lazy = true,
   dependencies = { 'MunifTanjim/nui.nvim', },
-  opts = {},
+  opts = {
+    companion_port = 12172,
+    run_command = {
+      python = { exec = 'pypy', args = { "$(FNAME)" } },
+    },
+    received_files_extension = "py",
+  },
 }
