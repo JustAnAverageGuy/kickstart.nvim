190cf92ed7c0150279de5953c3fa0b864baf8b59
JustAnAverageGuy <68919330+JustAnAverageGuy@users.noreply.github.com>
2023-12-27 02:06:20 +0530

    Add codeforces submitter

--- lua/custom/plugins/codeforces_submitter.lua
+++ lua/custom/plugins/codeforces_submitter.lua
@@ -1,4 +1,9 @@
 return
 {
-  "JustAnAverageGuy/CodeForce.vim"
+  -- "JustAnAverageGuy/CodeForce.vim",
+  dir="/home/aks/Tools/codeforces_submitter_vim",
+  name="codeforces_submitter.vim",
+  keys = {
+      {"<M-C-S>", "<cmd>CodeForceSubmit<cr>", {'n'}, desc="Submit code to codeforces, needs to have problem url in the source"},
+    }
 }
