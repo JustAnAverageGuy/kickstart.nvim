44943837c2ae82885689fc0423d78653c64e83c7
JustAnAverageGuy <68919330+JustAnAverageGuy@users.noreply.github.com>
2023-12-30 17:11:18 +0530

    add some extensions

---
+++ lua/custom/plugins/pets.lua
@@ -0,0 +1,21 @@
+return {
+  "giusgad/pets.nvim",
+  dependencies = { "MunifTanjim/nui.nvim", "giusgad/hologram.nvim" },
+  lazy=true,
+  -- cmd={
+  --   "PetsNew",
+  --   "PetsNewCustom",
+  --   "PetsList",
+  --   "PetsKill",
+  --   "PetsKillAll",
+  --   "PetsRemove",
+  --   "PetsRemoveAll",
+  --   "PetsPauseToggle",
+  --   "PetsHideToggle",
+  --   "PetsIdleToggle",
+  --   "PetsSleepToggle",
+  -- },
+  -- keys={
+  --   {'<leader>pets', "PetsNew",desc="Make a new pet"},
+  -- },
+}
