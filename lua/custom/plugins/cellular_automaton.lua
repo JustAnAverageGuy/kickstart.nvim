1ad780fd813abe38437b808efc824c75dd072765
JustAnAverageGuy <68919330+JustAnAverageGuy@users.noreply.github.com>
2023-12-30 17:11:18 +0530

    add some extensions

---
+++ lua/custom/plugins/cellular_automaton.lua
@@ -0,0 +1,10 @@
+return {
+  "Eandrju/cellular-automaton.nvim",
+  name="celullar automaton",
+  cmds={
+    {"CellularAutomaton make_it_rain", desc="Makes it rain"},
+  },
+  keys={
+    {"<leader>fml", "<cmd>CellularAutomaton make_it_rain<cr>", {"n"},desc="make it rain"},
+  }
+}
