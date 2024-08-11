e9319637b6d4c31b824f392bff63ad6a9b803873
JustAnAverageGuy <68919330+JustAnAverageGuy@users.noreply.github.com>
2024-08-11 22:58:50 +0530

    Add loremipsum to html lin luasnip

--- lua/custom/plugins/luasnip.lua
+++ lua/custom/plugins/luasnip.lua
@@ -3,10 +3,17 @@
 return {
     'L3MON4D3/LuaSnip',
 
-    config = function ()
-        require('luasnip.loaders.from_vscode').load({
+    -- Adds a number of user-friendly snippets
+    dependencies = {
+        'rafamadriz/friendly-snippets',
+    },
+
+    config = function()
+        require('luasnip.loaders.from_vscode').lazy_load({
             paths = './snippets'
-        })
+        });
+        require('luasnip.loaders.from_vscode').lazy_load();
+        require('luasnip').filetype_extend("html", {"loremipsum"});
     end
 
 }
