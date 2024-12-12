9267e26d4ea85bd33218bbf80d946922e66ba9a4
JustAnAverageGuy <68919330+JustAnAverageGuy@users.noreply.github.com>
2024-12-13 00:38:50 +0530

    Add undotree, treewalker, vim-brainfuck

---
+++ lua/custom/plugins/treewalker.lua
@@ -0,0 +1,13 @@
+return {
+  "aaronik/treewalker.nvim",
+  opts = {
+    highlight = true -- default is false
+  },
+  keys = {
+    { "<Up>",    "<cmd>Treewalker Up<CR>",    { "n" }, desc = "Treewalker up" },
+    { "<Down>",  "<cmd>Treewalker Down<CR>",  { "n" }, desc = "Treewalker down" },
+    { "<Right>", "<cmd>Treewalker Right<CR>", { "n" }, desc = "Treewalker right" },
+    { "<Left>",  "<cmd>Treewalker Left<CR>",  { "n" }, desc = "Treewalker left" },
+  },
+}
+
