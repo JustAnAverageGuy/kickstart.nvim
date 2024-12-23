59deb344c9d7f76dea7d9a4c187df76f6cc0de75
JustAnAverageGuy <68919330+JustAnAverageGuy@users.noreply.github.com>
2024-12-24 01:26:28 +0530

    Move markdown related plugins into markdown.lua

---
+++ lua/custom/plugins/markdown.lua
@@ -0,0 +1,10 @@
+return {
+  {"ellisonleao/glow.nvim", config = true, cmd = "Glow"},
+  {
+    'MeanderingProgrammer/render-markdown.nvim',
+    dependencies = { 'nvim-treesitter/nvim-treesitter', 'echasnovski/mini.icons' }, -- if you use standalone mini plugins
+    ---@module 'render-markdown'
+    ---@type render.md.UserConfig
+    opts = {},
+}
+}
