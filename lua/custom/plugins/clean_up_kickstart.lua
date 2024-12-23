b2f2ef34e9eb9dc2b562cd496caa5fb655ed98c2
JustAnAverageGuy <68919330+JustAnAverageGuy@users.noreply.github.com>
2024-12-24 05:29:40 +0530

    Extract treesitter into separate file
    TODO: Learn about treesitter properly. Maybe so that use swap
    capabilities
    TODO: extract lsp and completion stuff (maybe learn about blink.cmp)

--- lua/custom/plugins/clean_up_kickstart.lua
+++ lua/custom/plugins/clean_up_kickstart.lua
@@ -47,14 +47,4 @@ return {
     },
   },
 
-
-  {
-    -- Highlight, edit, and navigate code
-    'nvim-treesitter/nvim-treesitter',
-    dependencies = {
-      'nvim-treesitter/nvim-treesitter-textobjects',
-    },
-    build = ':TSUpdate',
-  },
-
 }
