7062b7aff6d06efae12f11064b9ccd7842a2cc8c
JustAnAverageGuy <68919330+JustAnAverageGuy@users.noreply.github.com>
2024-12-24 00:49:38 +0530

    Move all fun plugins into a single file, instead of separated directory

---
+++ lua/custom/plugins/fun_plugins.lua
@@ -0,0 +1,16 @@
+return {
+  {
+    "Eandrju/cellular-automaton.nvim",
+    name = "celullar_automaton",
+    cmd = { "CellularAutomaton", },
+    -- keys={
+    --   {"<leader>fml", "<cmd>CellularAutomaton make_it_rain<cr>", {"n"},desc="make it rain"},
+    -- }
+  },
+  {
+    "letieu/hacker.nvim",
+    opts = {},
+    -- keys = { { '<leader>hk', '<cmd>HackFollow<CR>', { 'n' }, desc = "the hackerman experience" } },
+    cmd = { "Hack", "HackAuto", "HackFollowAuto", "HackFollow" },
+  },
+}
--- lua/custom/plugins/hackerman.lua
+++
@@ -1,6 +0,0 @@
-return {
-  "letieu/hacker.nvim",
-  opts = {},
-  -- keys = { { '<leader>hk', '<cmd>HackFollow<CR>', { 'n' }, desc = "the hackerman experience" } },
-  cmd = { "Hack", "HackAuto", "HackFollowAuto", "HackFollow" },
-}
