1e466045c3c2b934bfec60ca460bf2fea5069bd6
JustAnAverageGuy <68919330+JustAnAverageGuy@users.noreply.github.com>
2024-12-24 01:22:47 +0530

    Fix adding unnecessary brackets to functions in autopair

--- lua/custom/plugins/autopairs.lua
+++ lua/custom/plugins/autopairs.lua
@@ -3,15 +3,17 @@
 return {
   "windwp/nvim-autopairs",
   -- Optional dependency
+  event = "InsertEnter",
   dependencies = { 'hrsh7th/nvim-cmp' },
-  config = function()
-    require("nvim-autopairs").setup {}
-    -- If you want to automatically add `(` after selecting a function or method
-    local cmp_autopairs = require('nvim-autopairs.completion.cmp')
-    local cmp = require('cmp')
-    cmp.event:on(
-      'confirm_done',
-      cmp_autopairs.on_confirm_done()
-    )
-  end,
+  opts = {},
+  --   -- If you want to automatically add `(` after selecting a function or method
+  -- config = function()
+  --   require("nvim-autopairs").setup {}
+  --   local cmp_autopairs = require('nvim-autopairs.completion.cmp')
+  --   local cmp = require('cmp')
+  --   cmp.event:on(
+  --     'confirm_done',
+  --     cmp_autopairs.on_confirm_done()
+  --   )
+  -- end,
 }
