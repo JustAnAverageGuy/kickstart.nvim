44b02533c0123194c500759a6e9fd1df03d04d26
JustAnAverageGuy <68919330+JustAnAverageGuy@users.noreply.github.com>
2024-12-25 03:14:38 +0530

    Change some statusline settings
    This may  break competitest, because I don't know why was a function
    returning that bufferlocal competitest_title. I had copied in from that
    one issue on the competitest repository, but on reading lualine's docs,
    it seems unnecessary

--- lua/custom/plugins/statusline.lua
+++ lua/custom/plugins/statusline.lua
@@ -1,33 +1,32 @@
 return {
-    -- Set lualine as statusline
-    'nvim-lualine/lualine.nvim',
-    -- See `:help lualine.txt`
-    opts = {
-      options = {
-        -- icons_enabled = false,
-        -- theme = 'catppuccin-mocha',
-        theme = 'dracula',
-        -- component_separators = '|',
-        -- section_separators = '',
-      },
-      extensions = {
-        {
-          filetypes = { 'CompetiTest' },
-          sections = {
-            lualine_b = { function()
-              ---@diagnostic disable-next-line: undefined-field
-              return vim.b.competitest_title or 'CompetiTest'
-            end },
-            lualine_y = { 'searchcount' },
-            lualine_z = { "change this sometime" },
-          },
-          inactive_sections = {
-            lualine_b = { function()
-              ---@diagnostic disable-next-line: undefined-field
-              return vim.b.competitest_title or 'CompetiTest'
-            end },
-          },
-        } -- CompetiTest statusline
-      }
+  -- Set lualine as statusline
+  'nvim-lualine/lualine.nvim',
+  -- See `:help lualine.txt`
+  opts = {
+    options = {
+      -- icons_enabled = false,
+      -- theme = 'catppuccin-mocha',
+      theme = 'dracula',
+      -- component_separators = '|',
+      -- section_separators = '',
     },
-  }
+    extensions = {
+      'quickfix',
+      'fugitive',
+      'lazy',
+      'man',
+      'mason',
+      {
+        filetypes = { 'CompetiTest' },
+        sections = {
+          lualine_b = { vim.b.competitest_title or 'CompetiTest' },
+          lualine_y = { 'searchcount' },
+          lualine_z = { "change this sometime" },
+        },
+        inactive_sections = {
+          lualine_b = { vim.b.competitest_title or 'CompetiTest' },
+        },
+      } -- CompetiTest statusline
+    }
+  },
+}
