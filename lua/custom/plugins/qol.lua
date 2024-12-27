1028595e7df896e9add71f494f0e07f2fc8aeb50
JustAnAverageGuy <68919330+JustAnAverageGuy@users.noreply.github.com>
2024-12-27 17:52:22 +0530

    Move autopairs to qol.lua

--- lua/custom/plugins/qol.lua
+++ lua/custom/plugins/qol.lua
@@ -130,4 +130,28 @@ return {
     "mbbill/undotree",
     cmd = { "UndotreeToggle", "UndotreeShow" },
   },
+  --[[
+   _       _       ___      _
+  /_\ _  _| |_ ___| _ \__ _(_)_ _
+ / _ \ || |  _/ _ \  _/ _` | | '_|
+/_/ \_\_,_|\__\___/_| \__,_|_|_|
+
+ ]]
+  {
+    "windwp/nvim-autopairs",
+    -- Optional dependency
+    event = "InsertEnter",
+    dependencies = { 'hrsh7th/nvim-cmp' },
+    opts = {},
+    --   -- If you want to automatically add `(` after selecting a function or method
+    -- config = function()
+    --   require("nvim-autopairs").setup {}
+    --   local cmp_autopairs = require('nvim-autopairs.completion.cmp')
+    --   local cmp = require('cmp')
+    --   cmp.event:on(
+    --     'confirm_done',
+    --     cmp_autopairs.on_confirm_done()
+    --   )
+    -- end,
+  },
 }
