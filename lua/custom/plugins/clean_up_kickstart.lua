580e400f5c8f575794d3f22599f8aad440b8bb69
JustAnAverageGuy <68919330+JustAnAverageGuy@users.noreply.github.com>
2024-12-27 17:25:22 +0530

    Move completion to separate file

--- lua/custom/plugins/clean_up_kickstart.lua
+++ lua/custom/plugins/clean_up_kickstart.lua
@@ -19,31 +19,14 @@ return {
     'neovim/nvim-lspconfig',
     dependencies = {
       -- Automatically install LSPs to stdpath for neovim
-      { 'williamboman/mason.nvim', config = true, },
+      { 'williamboman/mason.nvim', opts = {ui = {border = "single"} }, },
       'williamboman/mason-lspconfig.nvim',
       'WhoIsSethDaniel/mason-tool-installer.nvim',
 
       -- Useful status updates for LSP
       -- NOTE: `opts = {}` is the same as calling `require('fidget').setup({})`
       { 'j-hui/fidget.nvim',       opts = {} },
-
-    },
-  },
-
-  {
-    -- Autocompletion
-    'hrsh7th/nvim-cmp',
-    dependencies = {
-      -- Snippet Engine & its associated nvim-cmp source
-      'L3MON4D3/LuaSnip',
-      'saadparwaiz1/cmp_luasnip',
-
-      -- Adds LSP completion capabilities
       'hrsh7th/cmp-nvim-lsp',
-      'hrsh7th/cmp-path',
-
-      -- Adds a number of user-friendly snippets
-      'rafamadriz/friendly-snippets',
     },
   },
 
