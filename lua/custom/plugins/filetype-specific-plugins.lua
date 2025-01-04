da5fd3b93774d59cb38766d103e6e578ed043e05
JustAnAverageGuy <68919330+JustAnAverageGuy@users.noreply.github.com>
2025-01-04 22:15:16 +0530

    Move markdown related plugins into filetype-specific-plugins.lua

--- lua/custom/plugins/filetype-specific-plugins.lua
+++ lua/custom/plugins/filetype-specific-plugins.lua
@@ -6,5 +6,38 @@ return {
   {
     "fladson/vim-kitty",
     ft = "kitty",
+  },
+  --[[
+ __  __          _      _
+|  \/  |__ _ _ _| |____| |_____ __ ___ _
+| |\/| / _` | '_| / / _` / _ \ V  V / ' \
+|_|  |_\__,_|_| |_\_\__,_\___/\_/\_/|_||_|
+
+    ]]
+  {
+    "ellisonleao/glow.nvim",
+    ---@module 'glow'
+    ---@type Config
+    opts = {
+      border = "single",
+    },
+    cmd = "Glow"
+  },
+  {
+    'MeanderingProgrammer/render-markdown.nvim',
+    dependencies = { 'nvim-treesitter/nvim-treesitter', 'echasnovski/mini.icons' }, -- if you use standalone mini plugins
+    ---@module 'render-markdown'
+    ---@type render.md.UserConfig
+    opts = {
+      code = {
+        position  = 'right',
+        width     = 'block',
+        left_pad  = 1,
+        right_pad = 1,
+        border    = 'thick', -- TODO: doesn't seem to do anything, see later
+      }
+    },
+    ft = "markdown",
+    cmd = "RenderMarkdown",
   }
 }
