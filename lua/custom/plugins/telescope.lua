e3b3f6898b445da860840c5de07fd2ac31f2852d
JustAnAverageGuy <68919330+JustAnAverageGuy@users.noreply.github.com>
2025-01-07 01:38:32 +0530

    refactor(telescope): Use recipe from telescope wiki for "search on git root"
    Use this recipe: https://github.com/nvim-telescope/telescope.nvim/wiki/Configuration-Recipes/87a4df9f66be884f8cbfb3239c6f70b1f0462460#live-grep-from-project-git-root-with-fallback

--- lua/custom/plugins/telescope.lua
+++ lua/custom/plugins/telescope.lua
@@ -77,37 +77,32 @@ return {
       builtin.find_files { hidden = true, no_ignore = true, no_ignore_parent = true, }
     end, { desc = '[S]earch [A]ll files in cwd' })
 
-    local function find_git_root()
-      -- Use the current buffer's path as the starting point for the git search
-      local current_file = vim.api.nvim_buf_get_name(0)
-      local current_dir
-      local cwd = vim.fn.getcwd()
-      -- If the buffer is not associated with a file, return nil
-      if current_file == '' then
-        current_dir = cwd
-      else
-        -- Extract the directory from the current file's path
-        current_dir = vim.fn.fnamemodify(current_file, ':h')
-      end
-
-      -- Find the Git root directory from the current file's path
-      local git_root = vim.fn.systemlist(
-        'git -C ' .. vim.fn.escape(current_dir, ' ') .. ' rev-parse --show-toplevel'
-      )[1]
-      if vim.v.shell_error ~= 0 then
-        vim.notify('Not a git repository. Searching on current working directory', vim.log.WARN)
-        return cwd
-      end
-      return git_root
-    end
-
     -- Custom live_grep function to search in git root
-    local function live_grep_git_root()
-      local git_root = find_git_root()
-      if git_root then
-        builtin.live_grep { search_dirs = { git_root }, }
+    -- https://github.com/nvim-telescope/telescope.nvim/wiki/Configuration-Recipes/87a4df9f66be884f8cbfb3239c6f70b1f0462460#live-grep-from-project-git-root-with-fallback
+    local function live_grep_from_project_git_root()
+      local function is_git_repo()
+        vim.fn.system("git rev-parse --is-inside-work-tree")
+        return vim.v.shell_error == 0
       end
+
+      local function get_git_root()
+        local dot_git_path = vim.fn.finddir(".git", ".;")
+        return vim.fn.fnamemodify(dot_git_path, ":h")
+      end
+
+      local opts = {}
+
+      if is_git_repo() then
+        opts = {
+          cwd = get_git_root(),
+        }
+      else
+        vim.notify("Not a git repo", vim.log.levels.WARN)
+      end
+
+      require("telescope.builtin").live_grep(opts)
     end
-    vim.keymap.set('n', '<leader>sG', live_grep_git_root, { desc = '[S]earch by [G]rep on Git Root' })
+
+    vim.keymap.set('n', '<leader>sG', live_grep_from_project_git_root, { desc = '[S]earch by [G]rep on Git Root' })
   end
 }
