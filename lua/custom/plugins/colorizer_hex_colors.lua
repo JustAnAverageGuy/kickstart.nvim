5fcd84b74e5e46262b1163a393fd57a899ee4ffc
JustAnAverageGuy <68919330+JustAnAverageGuy@users.noreply.github.com>
2025-05-04 16:26:27 +0530

    Add baleia.nvim for better ANSI escape highlighting

--- lua/custom/plugins/colorizer_hex_colors.lua
+++ lua/custom/plugins/colorizer_hex_colors.lua
@@ -1,11 +1,29 @@
--- see :h colorizer.lua
-return {
-  'norcalli/nvim-colorizer.lua',
-  opts = {
-    ['css']  = { css = true, },
-    ['html'] = { css = true, },
-    'js',
-  }, -- NOTE: cannot use the second "default" argument this way, but whatever
-  ft = { 'css', 'html', 'js' },
-  cmd = { 'ColorizerToggle' }
+return
+{
+  {
+    -- see :h colorizer.lua
+    'norcalli/nvim-colorizer.lua',
+    opts = {
+      ['css']  = { css = true, },
+      ['html'] = { css = true, },
+      'js',
+    }, -- NOTE: cannot use the second "default" argument this way, but whatever
+    ft = { 'css', 'html', 'js' },
+    cmd = { 'ColorizerToggle' }
+  },
+  {
+    "m00qek/baleia.nvim",
+    version = "*",
+    config = function()
+      vim.g.baleia = require("baleia").setup({})
+
+      -- Command to colorize the current buffer
+      vim.api.nvim_create_user_command("BaleiaColorize", function()
+        vim.g.baleia.once(vim.api.nvim_get_current_buf())
+      end, { bang = true })
+
+      -- Command to show logs
+      vim.api.nvim_create_user_command("BaleiaLogs", vim.g.baleia.logger.show, { bang = true })
+    end,
+  }
 }
