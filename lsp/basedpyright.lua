9bbfbe6f0bac924125cb424d1e84a27d5b0640ba
JustAnAverageGuy <68919330+JustAnAverageGuy@users.noreply.github.com>
2025-08-03 06:23:26 +0530

    Update some lspconfig configs

--- lsp/basedpyright.lua
+++ lsp/basedpyright.lua
@@ -4,6 +4,19 @@
 ---
 --- `basedpyright`, a static type checker and language server for python
 
+-- This function resolves a difference between neovim nightly (version 0.11) and stable (version 0.10)
+---@param client vim.lsp.Client
+---@param method vim.lsp.protocol.Method
+---@param params? table some lsp support methods only in specific files
+---@return boolean
+local function client_notify(client, method, params)
+  if vim.fn.has 'nvim-0.11' == 1 then
+    return client:notify(method, params)
+  else
+    return client.notify(method, params)
+  end
+end
+
 local function set_python_path(path)
   local clients = vim.lsp.get_clients {
     bufnr = vim.api.nvim_get_current_buf(),
@@ -15,7 +28,7 @@ local function set_python_path(path)
     else
       client.config.settings = vim.tbl_deep_extend('force', client.config.settings, { python = { pythonPath = path } })
     end
-    client.notify('workspace/didChangeConfiguration', { settings = nil })
+    client_notify(client, 'workspace/didChangeConfiguration', { settings = nil })
   end
 end
 
@@ -37,6 +50,10 @@ return {
         autoSearchPaths = true,
         useLibraryCodeForTypes = true,
         diagnosticMode = 'openFilesOnly',
+
+        -- [[ Custom Config ]]
+        -- autoImportCompletions =  false,
+        typeCheckingMode = "standard",
       },
     },
   },
--- lsp/lua_ls.lua
+++ lsp/lua_ls.lua
@@ -80,4 +80,12 @@ return {
     'selene.yml',
     '.git',
   },
+  settings = {
+    Lua = {
+      -- workspace = { checkThirdParty = false },
+      telemetry = { enable = false },
+      -- NOTE: toggle below to ignore Lua_LS's noisy `missing-fields` warnings
+      diagnostics = { disable = { 'missing-fields' } },
+    }
+  },
 }
