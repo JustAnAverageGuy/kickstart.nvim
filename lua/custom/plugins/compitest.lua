ee239aec7a9f86e1ac566478fe7dacf9b7b153da
JustAnAverageGuy <68919330+JustAnAverageGuy@users.noreply.github.com>
2025-08-20 02:57:01 +0530

    competitest Change run trigger to leader jj

--- lua/custom/plugins/compitest.lua
+++ lua/custom/plugins/compitest.lua
@@ -7,23 +7,24 @@ return {
 		runner_ui = {
 			interface = "split",
 			mappings = {
-				run_all_again = "<leader>ck"
+				run_all_again = "<leader>jj"
 			}
 		},
 		run_command = {
-			python = { exec = '/usr/bin/pypy3', args = { "$(FNAME)" } },
+			python = { exec = 'pypy3', args = { "$(FNAME)" } },
 		},
 		received_files_extension = "py",
 		received_problems_path = function(task, file_extension)
 			-- local default_path = "$(CWD)/$(PROBLEM).$(FEXT)";
-			local beginning = require("competitest.receive").eval_receive_modifiers("$(CWD)/", task,
-				file_extension, true);
-			local problem_name_with_spaces = require("competitest.receive").eval_receive_modifiers(
-			"$(PROBLEM)", task, file_extension, true) or "this_didnt_match_problem_name";
-			local problem_name_without_spaces = string.gsub(problem_name_with_spaces, '[<>:"/\\|?* %.-]', "_"); -- strips out spaces and other special characters and replaces them with underscores
-			-- assert(vim.fn.has("win32") ~= 1, "The received problem path will be wrong");
+			local cwd = vim.fn.getcwd()
+			local problem_name_with_spaces = task.name
+			local problem_name_without_spaces = string.gsub(
+				problem_name_with_spaces,
+				'[<>:"/\\|?* %.-]', "_"
+			); -- strips out spaces and other special characters and replaces them with underscores
+			assert(vim.fn.has("win32") ~= 1, "The received problem path may be wrong");
 			-- WARN: This should not be a problem since paths are handled by lua, so using '/' even in windows etc should work
-			return beginning .. problem_name_without_spaces .. '.' .. file_extension
+			return cwd .. '/' .. problem_name_without_spaces .. '.' .. file_extension
 		end,
 		testcases_directory = "./testcases/",
 		template_file = {
@@ -68,7 +69,7 @@ return {
 			desc = "CompetiTest add custom testcases"
 		},
 		{
-			"<leader>ck",
+			"<leader>jj",
 			"<cmd>CompetiTest run<cr>",
 			{ "n" },
 			desc = "CompetiTest run the testcases",
