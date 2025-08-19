221a7efc3c21d9646648a4c6169451169de0339a
JustAnAverageGuy <68919330+JustAnAverageGuy@users.noreply.github.com>
2025-08-20 02:56:21 +0530

    Change file tree toggle to '\'

--- lua/custom/plugins/filetree.lua
+++ lua/custom/plugins/filetree.lua
@@ -1,6 +1,8 @@
 -- Unless you are still migrating, remove the deprecated commands from v1.x
 vim.g.neo_tree_remove_legacy_commands = 1
 
+  ---@module "lazy"
+  ---@type LazyPluginSpec
 return {
   "nvim-neo-tree/neo-tree.nvim",
   version = "*",
@@ -10,8 +12,19 @@ return {
     "MunifTanjim/nui.nvim",
   },
   keys = {
-    { "<leader>\\", "<cmd>Neotree toggle<cr>", desc = "NeoTree" },
+    { "\\", "<cmd>Neotree toggle<cr>", desc = "NeoTree" },
+    { "<leader>\\", "<cmd>Neotree reveal<cr>", desc = "NeoTree show in directory" },
   },
-  opts = {},
+  lazy = false,
+  ---@module "neo-tree"
+  ---@type neotree.Config?
+  opts = {
+    filesystem = {
+      window = {
+        mappings = {
+          ['\\'] = 'close_window',
+        },
+      },
+    }
+  },
 }
-
