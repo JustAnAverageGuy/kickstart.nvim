50a6581f6db81394d300faf86a7e7a1300ceaef7
JustAnAverageGuy <68919330+JustAnAverageGuy@users.noreply.github.com>
2025-08-20 12:17:57 +0530

    Move config requires from init.lua to config/init.lua

--- init.lua
+++ init.lua
@@ -1,20 +1,6 @@
---  And then you can explore or search through `:help lua-guide`
+-- You can explore or search through `:help lua-guide`
 -- https://neovim.io/doc/user/lua-guide.html
 
---  NOTE: Must happen before plugins are required (otherwise wrong leader will be used)
--- [[ Setting options ]]
-require("custom.config.options")
-
--- [[ Basic Keymaps ]]
-require("custom.config.keymaps")
-
--- [[ Configure diagnostics ]]
-require("custom.config.diagnostics")
-
--- [[ Install `lazy.nvim` plugin manager ]]
-require("custom.config.lazy")
-
--- [[ Configure Language Servers ]]
-require("custom.config.lsp")
+require "custom.config"
 
 -- vim: ts=2 sts=2 sw=2 et
---
+++ lua/custom/config/init.lua
@@ -0,0 +1,19 @@
+
+--
+--  NOTE: Must happen before plugins are required  otherwise wrong leader will be used
+--
+-- [[ Setting options ]]
+require "custom.config.options"
+
+-- [[ Basic Keymaps ]]
+require "custom.config.keymaps"
+
+-- [[ Configure diagnostics ]]
+require "custom.config.diagnostics"
+
+-- [[ Install `lazy.nvim` plugin manager ]]
+require "custom.config.lazy"
+
+-- [[ Configure Language Servers ]]
+require "custom.config.lsp"
+
